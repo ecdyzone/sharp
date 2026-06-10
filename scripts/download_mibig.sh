f72e2975d03c16d765afb56513039805a7187cfb
mumaral <mumaral@protonmail.com>
2026-06-10 17:24:02 -0300

    feat(mibig): create download_mibig.sh v1
    automatic downloading of mibig database
    the script works
    but for now destination folders are incorrect
    (they don't end in data/raw)

---
+++ scripts/download_mibig.sh
@@ -0,0 +1,16 @@
+# FILES DOWNLOADED FROM:
+# https://mibig.secondarymetabolites.org/download
+#
+# Version 4.0 (November 15, 2024)
+# All entries in JSON format.
+# All entries in GBK format.
+# All genes from MIBiG entries in FASTA format (amino acid sequences).
+
+# download files
+wget https://dl.secondarymetabolites.org/mibig/mibig_json_4.0.tar.gz
+wget https://dl.secondarymetabolites.org/mibig/mibig_gbk_4.0.tar.gz
+wget https://dl.secondarymetabolites.org/mibig/mibig_prot_seqs_4.0.fasta
+
+# extract tarballs
+tar -xzf mibig_json_4.0.tar.gz
+tar -xzf mibig_gbk_4.0.tar.gz
